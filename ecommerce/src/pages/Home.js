adaece340ee15701667f6891a1eaadb2321ef119
Kseniia Kondrat <oootlichno@gmail.com>
2024-11-15 15:30:03 -0600

    wip

--- ecommerce/src/pages/Home.js
+++ ecommerce/src/pages/Home.js
@@ -2,30 +2,33 @@ import React from "react";
 import leather from "../style/img/leather.png";
 import handtools from "../style/img/handtools.png";
 import molds from "../style/img/molds.png";
-
-
+import logo from "../style/img/logo.png";
 
 const HomePage = () => {
   return (
     <div>
-      <h1>Ecommerce Leather selling</h1>
-      <div>
-        <h2>SHOP BY CATEGORY</h2>
-        <div className="categories">
-          <div className="category">
-            <img src={leather} alt="category_picture" />
-            <p>Leather</p>
-          </div>
+      <div className="header">
+        <div className="logo">
+          <img src={logo} alt="logo" />
+        </div>
+      </div>
 
-          <div className="category">
-            <img src={handtools} alt="category_picture" />
-            <p>Hand Tools</p>
-          </div>
-
-          <div className="category">
-            <img src={molds} alt="category_picture" />
-            <p>Molds</p>
-          </div>
+      <div >
+      <h2>SHOP BY CATEGORY</h2>
+      </div>
+      
+      <div className="categories">
+        <div className="category">
+          <img src={leather} alt="Leather" />
+          <p>Leather</p>
+        </div>
+        <div className="category">
+          <img src={handtools} alt="Hand Tools" />
+          <p>Hand Tools</p>
+        </div>
+        <div className="category">
+          <img src={molds} alt="Molds" />
+          <p>Molds</p>
         </div>
       </div>
     </div>
